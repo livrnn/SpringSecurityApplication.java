1c4d606394e6d4713ef337c6ddf5d6fa6a68a4e7
livrnn <lizavoronina03@mail.ru>
2023-05-21 19:45:15 +0300

    Итоговая

---
+++ src/main/java/com/example/springsecurityapplication/controllers/AuthenticationController.java
@@ -0,0 +1,15 @@
+package com.example.springsecurityapplication.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthenticationController {
+
+
+    @GetMapping("/authentication")
+    public String login(){
+        return "authentication";
+    }
+
+}
---
+++ src/main/java/com/example/springsecurityapplication/enumm/Status.java
@@ -0,0 +1,5 @@
+package com.example.springsecurityapplication.enumm;
+
+public enum Status {
+    Оформлен
+}
